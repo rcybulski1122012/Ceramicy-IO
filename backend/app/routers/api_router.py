3de4f60a242adf61f5a5e2ab33826d8d143f9367
Radosław Cybulski <68512922+rcybulski1122012@users.noreply.github.com>
2024-05-20 18:28:06 +0200

    [CI-26][CI-27] Basic file upload (#8)
    * feat: basic file upload without validation
    
    * feat: basic type validation & blob url fix

--- backend/app/routers/api_router.py
+++ backend/app/routers/api_router.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
-from app.routers.quiz import router as quiz_router
+
+from app.models.session import *  # noqa
 
 # TODO remove these imports - here to make sure the models are loaded to the registry
-from app.models.user import *   # noqa
-from app.models.session import *    # noqa
+from app.models.user import *  # noqa
+from app.routers.file import router as file_router
+from app.routers.quiz import router as quiz_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(quiz_router)
+api_router.include_router(file_router)
--- backend/app/routers/quiz.py
+++ backend/app/routers/quiz.py
@@ -1,11 +1,12 @@
+from fastapi import APIRouter, Depends
+
 from typing import Annotated, Sequence
 
-from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import get_session
 from app.models.quiz import Quiz
-from app.schemas.quiz import QuizOutList
+from app.schemas.quiz import QuizIn, QuizOut, QuizOutList
 from app.services import quiz as quiz_service
 
 router = APIRouter(tags=["Quiz"], prefix="/quiz")
@@ -14,3 +15,12 @@ router = APIRouter(tags=["Quiz"], prefix="/quiz")
 @router.get("/", response_model=list[QuizOutList])
 async def get_quizzes(session: Annotated[AsyncSession, Depends(get_session)]) -> Sequence[Quiz]:
     return await quiz_service.get_quizzes(session)
+
+
+@router.post("/", response_model=QuizOut)
+async def create_quiz(
+    session: Annotated[AsyncSession, Depends(get_session)],
+    quiz_in: QuizIn,
+) -> Quiz:
+    quiz = await quiz_service.create_quiz(session, quiz_in.model_dump())
+    return quiz
