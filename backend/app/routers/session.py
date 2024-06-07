e7e7a847a51f63d7bba07912f6e60b19cea58794
socz3qqq <soczawa@student.agh.edu.pl>
2024-06-07 20:17:20 +0200

    Added requested changes: * user session existance validation * removed user_name from url, and added it to request body * removed quiz_id from query parameters

--- backend/app/routers/session.py
+++ backend/app/routers/session.py
@@ -10,7 +10,7 @@ from app.db import get_session
 from app.models.session import Session
 from app.models.user import User
 from app.schemas.session import SessionIn, SessionJoinIn, SessionOut, UserSessionRankingOut
-from app.schemas.quiz_check import QuizCheckIn, QuizCheckOut
+from app.schemas.quiz_check import UserQuizSolutionIn, QuizCheckOut
 from app.services.user import get_current_user
 
 router = APIRouter(tags=["Session"], prefix="/session")
@@ -56,13 +56,12 @@ async def get_session_ranking(
     return await session_service.get_session_ranking(db_session, session_id)
 
 
-@router.post("/{session_id}/ranking/{user_name}/submit", response_model=QuizCheckOut)
+@router.post("/{session_id}/solution/submit", response_model=QuizCheckOut)
 async def check_solution(
     db_session: Annotated[AsyncSession, Depends(get_session)],
     session_id: str,
-    user_name: str,
-    quiz_id: str,
-    files_in: QuizCheckIn,
+    files_in: UserQuizSolutionIn,
 ) -> QuizCheckOut:
-    await session_service.asign_solution_to_user_session(db_session, session_id, user_name)
-    return await quiz_check_service.check_files(db_session, quiz_id, files_in)
+    await session_service.assign_solution_to_user_session(db_session, session_id, files_in.user_name)
+    session = await session_service.get_session_by_id(db_session, session_id)
+    return await quiz_check_service.check_files(db_session, session.quiz_id, files_in)
--- backend/app/services/session.py
+++ backend/app/services/session.py
@@ -10,6 +10,7 @@ from app.schemas.session import UserSessionRankingOut
 from app.schemas.quiz_check import QuizCheckIn
 from app.services.quiz_check import check_files
 
+
 async def create_session(db_session: AsyncSession, quiz_id: str, current_user: User) -> Session:
     await get_quiz_by_id(db_session, quiz_id)
     session_obj = Session(quiz_id=quiz_id, host=current_user)
@@ -49,10 +50,11 @@ async def delete_session(db_session: AsyncSession, session_id: str, current_user
     await db_session.delete(session_obj)
     await db_session.commit()
 
+
 async def get_session_ranking(db_session: AsyncSession, session_id: str) -> list[UserSessionRankingOut]:
     session = await get_session_by_id(db_session, session_id)
     participants = await session.awaitable_attrs.participants
-    
+
     if not participants:
         return []
 
@@ -64,12 +66,27 @@ async def get_session_ranking(db_session: AsyncSession, session_id: str) -> list
         file_check_in = QuizCheckIn(files=submitted_files)
         quiz_check_out = await check_files(db_session, session.quiz_id, file_check_in)
 
-        rankings.append(UserSessionRankingOut(user_name=user_name, score=quiz_check_out.score, solution=submitted_files))
+        rankings.append(
+            UserSessionRankingOut(user_name=user_name, score=quiz_check_out.score, solution=submitted_files)
+        )
 
     rankings.sort(key=lambda x: x.score, reverse=True)
     return rankings
 
-async def asign_solution_to_user_session(db_session: AsyncSession, session_id: str, user_name: str, solution: QuizCheckIn) -> None:
+
+async def assign_solution_to_user_session(
+    db_session: AsyncSession, session_id: str, user_name: str, solution: QuizCheckIn
+) -> None:
+    result = await db_session.execute(
+        select(UserSession).filter(UserSession.session_id == session_id, UserSession.user_name == user_name)
+    )
+    user_session = result.scalar_one_or_none()
+    if user_session is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User Session not found. Please make you sure joined the session using the a valid link",
+        )
+
     statement = (
         update(UserSession)
         .where(UserSession.session_id == session_id, UserSession.user_name == user_name)
