01e72a20a4472aa4023a1568a5ed84a752218a7b
Filip Dziurdzia <fdziurdzia@student.agh.edu.pl>
2024-06-11 08:17:27 +0200

    file hierarchy done (cors problem with blob)

--- frontend/src/components/pages/quiz/FileSelection.tsx
+++ frontend/src/components/pages/quiz/FileSelection.tsx
@@ -1,19 +1,30 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, RadioGroup, VStack, Text, Radio } from '@chakra-ui/react';
 import { useTheme } from '@emotion/react';
-import { Quiz } from '../../../data/quizzes';
+import { Quiz, File } from '../../../data/quizzes';
+import { useState } from 'react';
 
 type FileSelectionProps = {
-  quiz: Quiz;
+  quiz: any;
   files: File[];
+  onFileSelect: (fileId: string) => void;
 };
 
-const FileSelection = ({ quiz, files }: FileSelectionProps) => {
+const FileSelection = ({ quiz, files, onFileSelect }: FileSelectionProps) => {
   const theme = useTheme();
-  files;
+  const [selectedFile, setSelectedFile] = useState<string>('0');
+
+  const isSelected = (_value: string | number) => _value == selectedFile;
+
+  const handleFileClick = (fileId: string) => {
+    setSelectedFile(fileId);
+    // onFileSelect(fileContent);
+  };
+  
   return (
     <Flex
       direction={'column'}
       align={'start'}
+      w={'100%'}
       py={6}
       pr={8}
       gap={4}
@@ -22,11 +33,59 @@ const FileSelection = ({ quiz, files }: FileSelectionProps) => {
       <Text fontFamily={'heading'} textStyle={'h2'}>
         {quiz.name}
       </Text>
-      <Text fontFamily={'body'}>
+      {/* <Text fontFamily={'body'}>
         {quiz.description || 'No description available'}
-      </Text>
+      </Text> */}
       <Box px={2} w={'100%'} mt={8}>
-        {/* TODO add FileList */}
+        <RadioGroup
+          w={'100%'}
+          onChange={handleFileClick}
+          value={selectedFile}
+        >
+          <VStack alignItems={'start'} w={'100%'}>
+            {files.map((file) => (
+              <Box
+                key={quiz.id}
+                w="100%"
+                borderLeftRadius={12}
+                _hover={{ background: 'green.10', color: 'black' }}
+                sx={
+                  isSelected(file.id)
+                    ? {
+                        background: 'green.50 !important',
+                        color: 'black',
+                        fontWeight: '900 !important',
+                        borderRight: `4px solid ${theme.colors.primary}`,
+                      }
+                    : {}
+                }
+              >
+                <Radio
+                  spacing={8}
+                  key={file.id}
+                  w={'100%'}
+                  px={5}
+                  py={3}
+                  value={`${file.id}`}
+                  _hover={{ background: 'red' }}
+                >
+                  <Flex
+                    justifyContent="space-between"
+                    alignItems="center"
+                    w="100%"
+                  >
+                    <Text fontFamily="heading" fontSize={14} fontWeight="bold" pr={'50%'}>
+                      {file.name}
+                    </Text>
+                    <Text fontFamily="heading" fontSize={18} fontWeight="bold">
+                      {'7' + '/' + '10'}
+                    </Text>
+                  </Flex>
+                </Radio>
+              </Box>
+            ))}
+          </VStack>
+        </RadioGroup>
       </Box>
     </Flex>
   );
--- frontend/src/components/pages/quiz/QuizPage.tsx
+++ frontend/src/components/pages/quiz/QuizPage.tsx
@@ -1,15 +1,84 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import Layout from '../../layout/Layout';
-import quizzes from '../../../data/quizzes';
+import { quizzes } from '../../../data/quizzes';
 import FileSelection from './FileSelection';
 import Editor from './Editor';
+import { useEffect, useState } from 'react';
 
-const QuizPage = () => {
+const QuizPage = (quiz_id: string) => {
+  const [selectedFileContent, setSelectedFileContent] = useState<string>('');
+  const [quizData, setQuizData] = useState<Array<{ id: string, name: string, url: string, smell_count: number }>>([]);
+  const [selectedFileId, setSelectedFileId] = useState<string | null>(null);
+
+  const process_quiz_data = (data: any) => {
+    console.log(data)
+    const fileUrls = data.file_urls;
+        const codeSmells = data.code_smells;
+        
+        const filesData = fileUrls.map((url: string, id: string) => {
+          const fileName = url.split('/').pop() || '';
+          const smellCount = codeSmells[url] ? codeSmells[url].length : 0;
+          return {
+            id: id,
+            name: fileName,
+            url: url,
+            smell_count: smellCount
+          };
+        });
+    return filesData;
+  };
+
+  useEffect(() => {
+    const fetchQuizData = async () => {
+      try {
+        quiz_id = 'dbeafc38-19ce-4438-be88-1d47e124ada0';
+        const response = await fetch(
+          `http://localhost:8000/api/v1/quiz/${quiz_id}`,
+        );
+        const data = await response.json();
+        let processed_data = process_quiz_data(data);
+        setQuizData(processed_data);
+        handleFileSelect(processed_data[0].id);
+      } catch (error) {
+        console.error('Error fetching quiz data:', error);
+      }
+    };
+
+    fetchQuizData();
+  }, [quiz_id]);
+
+  useEffect(() => {
+    const fetchFileContent = async () => {
+      if (selectedFileId !== null) {
+        const selectedFile = quizData.find(file => file.id === selectedFileId);
+        if (selectedFile) {
+          try {
+            const response = await fetch(selectedFile.url);
+            const fileContent = await response.text();
+            setSelectedFileContent(fileContent);
+          } catch (error) {
+            console.error('Error fetching file content:', error);
+          }
+        }
+      }
+    };
+
+    fetchFileContent();
+  }, [selectedFileId, quizData]);
+
+  const handleFileSelect = (fileId: string) => {
+    setSelectedFileId(fileId);
+  };
+
   return (
     <Layout>
       <Grid templateColumns="repeat(7, 1fr)" width={'100%'}>
         <GridItem colSpan={2} minWidth={'360px'}>
-          <FileSelection quiz={quizzes[0]} files={[]} />
+          <FileSelection
+            quiz={quizData.find(file => file.id === selectedFileId) || {id: '', name: ''}}
+            files={quizData}
+            onFileSelect={handleFileSelect}
+          />
         </GridItem>
         <GridItem colSpan={5} w="100%" p={6}>
           <Editor></Editor>
